75ec4a3795a91bdf32af63bef46a0f5eb17ec866
efi india <efiindia@mahaveeb-mbp.local>
2016-01-24 14:48:04 +0530

    with db migrate change

--- db/migrate/20151227061318_remove_project_id.rb
+++ db/migrate/20151227061318_remove_project_id.rb
@@ -1,7 +1,7 @@
 class RemoveProjectId < ActiveRecord::Migration
   def change
   	remove_column :project1_categories, :project_id
-  	add_column :project1_categories, :project1_id, :integer
+  	#add_column :project1_categories, :project1_id, :integer
 
 
   end
